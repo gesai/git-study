e212d5e1055e0b171e563bb94925d03a76419d1f
gesai <gesai@yxt.com>
2021-06-28 14:13:01 +0800

    111111111

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch 2')
+		},
+		onShow: function() {
+			console.log('App Show 2')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
